39fec700453451133bb872df61a9d6137cd04c77
tmsouthwell <goldtoneconsulting@gmail.com>
2022-01-31 14:51:15 -0800

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    if else
+        (i % 5 == 0) {
+            console.log("Buzz");
+        }
+    if else
+        (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+        }
+    else {
+        console.log(i);
+    }
+}
